a714765182dda3a7a8e1b9e7d534e023262cf67b
urba2z <54369458+urba2z@users.noreply.github.com>
2020-07-09 14:36:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,74 @@
 "use strict";
 
-const modalAdd = document.querySelector('.modal__add');
-const modalItem = document.querySelector('.modal__item');
+const dataBase = [];
+
 const addBtn = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
-const modalSubmit = document.querySelector('.modal__submit')
-const cards = document.querySelectorAll('.card')
+const modalSubmit = document.querySelector('.modal__submit');
+const catalog = document.querySelector('.catalog');
+const warning = document.querySelector('.modal__btn-warning')
 
+const elementsModalSubmit = [...modalSubmit]
+  .filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit')
 
-// Открытие окна. В качестве аргемента передается переменная модального окна
+// Открытие окна. В качестве аргемента передается селектор модального окна
 function openMoodal(modalName) {
-  modalName.classList.remove('hide')
-  document.body.style.overflow = "hidden"
+  const MODAL = document.querySelector(modalName)
+  MODAL.classList.remove('hide')
+  document.body.style.overflow = "hidden";
+  closeModal(modalName)
 }
 
-
-/* 
-- Закрытие окна. 
-- В качестве аргемента передается переменная модального окна. 
-- Без event-а не работает. ДОРАБОТАТЬ (коммент для себя)
-*/
-function closeModal(modalName, event) {
-  if (event.target.classList.contains('modal__close') ||
-    event.target === modalName
-  ) {
-    modalName.classList.add('hide');
-    document.body.style.overflow = "";
-    modalSubmit.reset(); // Сброс данных формы после закрытия окна
+// Закрытие окна. В качестве аргемента передается селектор модального окна
+function closeModal(modalName) {
+  const MODAL = document.querySelector(modalName)
+  const closeByEsc = event => {
+    if (event.key === 'Escape') {
+      MODAL.classList.add('hide')
+      document.body.style.overflow = "";
+      document.removeEventListener('keydown', closeByEsc)
+    }
   }
-
-}
-
-//Закрытие пнри нажатии "esc". В качестве аргемента передается переменная модального окна. 
-function closeByEsc(modalName) {
-  document.addEventListener('keydown', event => {
-    if (event.keyCode === 27) {
-      modalName.classList.add('hide')
-      document.body.style.overflow = ""
+  document.addEventListener('keydown', closeByEsc)
+  MODAL.addEventListener('click', event => {
+    if (event.target.classList.contains('modal__close') ||
+      event.target === MODAL
+    ) {
+      MODAL.classList.add('hide');
+      document.body.style.overflow = "";
+      modalSubmit.reset(); // Сброс данных формы после закрытия окна
     }
   })
 }
 
-
+// Модальное окно нового товара
 addBtn.addEventListener('click', () => {
-  openMoodal(modalAdd);
-  closeByEsc(modalAdd);
+  openMoodal('.modal__add');
   modalBtnSubmit.disabled = true
 });
 
-modalAdd.addEventListener('click', () => {
-  closeModal(modalAdd, event);
+//Открытие модального окна товара
+catalog.addEventListener('click', event => {
+  if (event.target.closest('.card')) {
+    openMoodal('.modal__item');
+  }
+})
+
+//Валидация
+modalSubmit.addEventListener('input', () => {
+  const valid = elementsModalSubmit.every(elem => elem.value)
+  modalBtnSubmit.disabled = !valid
+  warning.style.display = valid ? 'none' : 'block'
 });
 
-cards.forEach(item => {
-  item.addEventListener('click', () => {
-    openMoodal(modalItem);
-    closeByEsc(modalItem);
-  })
-});
-
-modalItem.addEventListener('click', () => {
-  closeModal(modalItem, event)
+//Новое объявление
+modalSubmit.addEventListener('submit', event => {
+  event.preventDefault();
+  const itemObj = {};
+  for (const elem of elementsModalSubmit) {
+    itemObj[elem.name] = elem.value
+  }
+  dataBase.push(itemObj);
+  modalSubmit.reset();
+  closeModal('.modal__add');
 })
